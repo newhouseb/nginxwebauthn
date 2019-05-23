ff95d6df3348db069f1a4026abe783a1c0d9fdaf
Ben Newhouse <newhouseb@gmail.com>
2019-05-23 12:29:40 -0400

    Added readme and requirements

--- main.py
+++ main.py
@@ -31,36 +31,41 @@ function barraytoa(arrayBuffer) {
 
 async function configure() {
     try {
-        let data = await fetch('/auth/setup', { method: 'POST' });
+        let data = await fetch('/auth/get_challenge_for_new_key', { method: 'POST' });
         let json = await data.json()
         json.publicKey.challenge = atobarray(json.publicKey.challenge)
         json.publicKey.user.id = atobarray(json.publicKey.user.id)
         let cred = await navigator.credentials.create(json)
-        window.command.innerHTML = 'python3 main.py save-client ' + window.location.host + ' ' + barraytoa(cred.response.clientDataJSON) + ' ' + barraytoa(cred.response.attestationObject)
+        window.command.innerHTML = 'On your server, to save this key please run:<br /><pre>python3 main.py save-client ' + window.location.host + ' ' + barraytoa(cred.response.clientDataJSON) + ' ' + barraytoa(cred.response.attestationObject) + '</pre>'
     } catch (e) {
         console.log(e)
     }
 }
 
 (async function init() {
-    let data = await fetch('/auth/begin', { method: 'POST' });
-    let json = await data.json()
-    if (json.publicKey !== undefined) {
-        json.publicKey.challenge = atobarray(json.publicKey.challenge)
-        json.publicKey.allowCredentials[0].id = atobarray(json.publicKey.allowCredentials[0].id)
-        let result = await navigator.credentials.get(json)
-        await fetch('/auth/complete', { method: 'POST', body: JSON.stringify({
-          id: barraytoa(result.rawId),
-          authenticatorData: barraytoa(result.response.authenticatorData),
-          clientDataJSON: barraytoa(result.response.clientDataJSON),
-          signature: barraytoa(result.response.signature)
-        }), headers:{ 'Content-Type': 'application/json' }})
-        console.log(result)
+    try {
+        let data = await fetch('/auth/get_challenge_for_existing_key', { method: 'POST' });
+        let json = await data.json()
+        if (json.publicKey !== undefined) {
+            json.publicKey.challenge = atobarray(json.publicKey.challenge)
+            json.publicKey.allowCredentials[0].id = atobarray(json.publicKey.allowCredentials[0].id)
+            let result = await navigator.credentials.get(json)
+            await fetch('/auth/complete_challenge_for_existing_key', { method: 'POST', body: JSON.stringify({
+              id: barraytoa(result.rawId),
+              authenticatorData: barraytoa(result.response.authenticatorData),
+              clientDataJSON: barraytoa(result.response.clientDataJSON),
+              signature: barraytoa(result.response.signature)
+            }), headers:{ 'Content-Type': 'application/json' }})
+            window.location.href = "/"
+        }
+        if (json.error == 'not_configured') {
+            configure();
+        }
+    } catch(e) {
+        window.configure.style.display = '';
     }
-
 })()
 </script>
-<button onclick="configure()">Configure</button>
 <div id="command"></div>
 </body>
 """
@@ -94,7 +99,6 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/auth/check':
             cookie = http.cookies.SimpleCookie(self.headers.get('Cookie'))
-            print(cookie, cookie['token'].value, TOKEN_MANAGER.tokens)
             if 'token' in cookie and TOKEN_MANAGER.is_valid(cookie['token'].value):
                 self.send_response(200)
                 self.end_headers()
@@ -111,6 +115,21 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(bytes(FORM, 'UTF-8'))
             return
 
+        if self.path == '/auth/logout':
+            cookie = http.cookies.SimpleCookie(self.headers.get('Cookie'))
+            if 'token' in cookie:
+                TOKEN_MANAGER.invalidate(cookie['token'].value)
+
+            # This just replaces the token with garbage
+            self.send_response(302)
+            cookie = http.cookies.SimpleCookie()
+            cookie["token"] = '***'
+            cookie["token"]["path"] = '/'
+            cookie["token"]["secure"] = True
+            self.send_header('Set-Cookie', cookie.output(header=''))
+            self.send_header('Location', '/')
+            self.end_headers()
+
         self.send_response(404)
         self.end_headers()
 
@@ -121,7 +140,7 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
         rp = RelyingParty(host, 'NGINX Auth Server')
         server = U2FFido2Server(origin, rp)
 
-        if self.path == "/auth/setup":
+        if self.path == "/auth/get_challenge_for_new_key":
             registration_data, state = server.register_begin({ 'id': b'default', 'name': "Default user", 'displayName': "Default user" })
             registration_data["publicKey"]["challenge"] = str(base64.b64encode(registration_data["publicKey"]["challenge"]), 'utf-8')
             registration_data["publicKey"]["user"]["id"] = str(base64.b64encode(registration_data["publicKey"]["user"]["id"]), 'utf-8')
@@ -135,12 +154,19 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(bytes(json.dumps(registration_data), 'UTF-8'))
             return
 
+        if not os.path.exists('.credentials'):
+            self.send_response(401)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'error': 'not_configured'}), 'UTF-8'))
+            return
+
         creds = []
         with open('.credentials', 'rb') as f:
             cred, _ = AttestedCredentialData.unpack_from(f.read())
             creds.append(cred)
 
-        if self.path == "/auth/begin":
+        if self.path == "/auth/get_challenge_for_existing_key":
             auth_data, state = server.authenticate_begin(creds)
             auth_data["publicKey"]["challenge"] = str(base64.b64encode(auth_data["publicKey"]["challenge"]), 'utf-8')
             auth_data["publicKey"]["allowCredentials"][0]["id"] = str(base64.b64encode(auth_data["publicKey"]["allowCredentials"][0]["id"]), 'utf-8')
@@ -152,7 +178,7 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(json.dumps(auth_data), 'UTF-8'))
 
-        if self.path == "/auth/complete":
+        if self.path == "/auth/complete_challenge_for_existing_key":
             data = json.loads(self.rfile.read(int(self.headers.get('Content-Length'))))
 
             credential_id = base64.b64decode(data['id'])
@@ -169,8 +195,6 @@ class AuthHandler(http.server.BaseHTTPRequestHandler):
                     auth_data,
                     signature
                 )
-
-            print("Auth ok!")
 
             cookie = http.cookies.SimpleCookie()
             cookie["token"] = TOKEN_MANAGER.generate()
---
+++ readme.md
@@ -0,0 +1,63 @@
+# NGINX + WebAuthn for your small scale web applications
+
+## What is this for?
+
+If you run some small services on a public-facing server that you would like to protect (i.e. Jupyter of VS code-server) and have a Yubikey or similar, you can use this repository to add secure, public-key authentication to it.
+
+## How?
+
+Set up NGINX to proxy your service, note that you will also need SSL because WebAuthn only works over HTTPS.  I highly recommend using letsencrypt + `certbot` so set up SSL:
+
+```
+server {
+    server_name myserver.bennewhouse.com; # managed by Certbot
+
+    # Redirect everything that begins with /auth to the authorization server
+    location /auth {
+        proxy_pass http://127.0.0.1:8000;
+    }
+
+    # If the authorization server returns 401 Unauthorized, redirect to /atuh/login
+    error_page 401 = @error401;
+    location @error401 {
+        return 302 /auth/login;
+    }
+
+    root /var/www/html;
+    index index.html;
+    location / {
+        auth_request /auth/check; # Ping /auth/check for every request, and if it returns 200 OK grant access
+      
+        # Here is where you would put other proxy_pass info to forward to Jupyter, etc. In this example I'm just serving raw HTML
+    }
+
+    listen [::]:443 ssl ; # managed by Certbot
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/myserver.bennewhouse.com/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/myserver.bennewhouse.com/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+```
+
+Reload NGINX with the aforementioned configuration. Next install the required depenencies (only one at the moment) and run main.py in a long-running fashion (either in `tmux`, `screen` or if you're fancy a systemd daemon)
+
+```
+pip3 install -r requirements.txt
+python3 main.py
+```
+
+Browse to your site on a page that supports WebAuthn (most things other than Safari). Insert your security key when requested, and the page will tell you to run a command that looks like:
+
+```
+python3 main.py save-client myserver.bennewhouse.com *big long base64 string* *big long base64 string*
+```
+
+Run that from the same place you've checked out this code. You only need to do this once to authorize your key.
+
+That's it! Navigating back to your website will now authenticate you using the key you just saved.
+
+## Limitations
+
+- At the moment, we only store one set of credentials. It'd be nice to store multiple credentials, especially across different domains.
+- This uses the built-in python3 server, which isn't designed for high-volume. You'd want to port this to a uwsgi setup if you wanted to productionize it. 
